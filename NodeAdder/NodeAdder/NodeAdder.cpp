b0d4f378a8e8fc01592377b73b9c0e7441f6e4dc
Eric Charnesky <echarnes@UMICH.EDU>
2016-10-25 17:32:16 -0400

    rename sum to currentNode

--- NodeAdder/NodeAdder/NodeAdder.cpp
+++ NodeAdder/NodeAdder/NodeAdder.cpp
@@ -12,45 +12,45 @@ struct Node
 Node add(Node* left, Node* right)
 {
 	Node *head = new Node;
-	Node *sum = head;
-	sum->e = 0;
-	sum->c = 0;
-	sum->next = nullptr;
+	Node *currentNode = head;
+	currentNode->e = 0;
+	currentNode->c = 0;
+	currentNode->next = nullptr;
 
 	if (left != nullptr)
 	{
-		sum->c = left->c;
-		sum->e = left->e;
-		sum->next = nullptr;
+		currentNode->c = left->c;
+		currentNode->e = left->e;
+		currentNode->next = nullptr;
 		// BAD!!! NOT A DEEP COPY sum->next = left->next;
 	
 		while (left->next != nullptr)
 		{
-			sum->next = new Node;
-			sum->next->c = left->next->c;
-			sum->next->e = left->next->e;
-			sum->next->next = nullptr;
+			currentNode->next = new Node;
+			currentNode->next->c = left->next->c;
+			currentNode->next->e = left->next->e;
+			currentNode->next->next = nullptr;
 			left = left->next;
-			sum = sum->next;
+			currentNode = currentNode->next;
 		}
 	}
 
 	if (right != nullptr)
 	{
-		sum->next = new Node;
-		sum->next->c = right->c;
-		sum->next->e = right->e;
-		sum->next->next = nullptr;
-		sum = sum->next;
+		currentNode->next = new Node;
+		currentNode->next->c = right->c;
+		currentNode->next->e = right->e;
+		currentNode->next->next = nullptr;
+		currentNode = currentNode->next;
 
 		while (right->next != nullptr)
 		{
-			sum->next = new Node;
-			sum->next->c = right->next->c;
-			sum->next->e = right->next->e;
-			sum->next->next = nullptr;
+			currentNode->next = new Node;
+			currentNode->next->c = right->next->c;
+			currentNode->next->e = right->next->e;
+			currentNode->next->next = nullptr;
 			right = right->next;
-			sum = sum->next;
+			currentNode = currentNode->next;
 		}
 
 	}
@@ -63,11 +63,6 @@ int main()
 	Node *first = new Node;
 	first->e = 1;
 	first->c = 2;
-
-	for (int i = 0; i < 10; i++)
-	{
-		Node *newNode = new Node;
-	}
 
 	Node *second = new Node;
 	second->e = 3;
